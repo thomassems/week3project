460d4756a91aa759348fa5f285ab725b7e7a54c3
catou88 <catherinezheng25@gmail.com>
2023-09-26 09:57:13 -0400

    API key problem addressed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,15 +29,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        String apiKey = "tjZ3LgBnocuKxzqJDIP8aQ==U01N8LuUqrlVhiNR"; // Replace with your actual API key
+        String apiKey = "KE8a7QjrGwSZ2jx3+4URNg==aPNxCl8ULpu4Trvb"; // Replace with your actual API key
 
         URL url = new URL("https://api.api-ninjas.com/v1/exercises?muscle=biceps");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
         connection.setRequestProperty("Accept", "application/json");
+        connection.addRequestProperty("X-Api-Key", apiKey);
         InputStream responseStream = connection.getInputStream();
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(responseStream);
-        System.out.println(root.path("fact").asText());
+        System.out.println(root);
     }
 }
